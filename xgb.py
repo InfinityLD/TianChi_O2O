8976a1405782ef512edaf8c1932a1c0110ea2c85
Liu dui <913641653@qq.com>
2018-08-21 16:05:17 +0800

    using XGBoost

---
+++ xgb.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+
+from sklearn.preprocessing import MinMaxScaler
+
+
+dataset1 = pd.read_csv('data/dataset1.csv')
+dataset1.label.replace(-1,0,inplace=True)
+dataset2 = pd.read_csv('data/dataset2.csv')
+dataset2.label.replace(-1,0,inplace=True)
+dataset3 = pd.read_csv('data/dataset3.csv')
+
+dataset1.drop_duplicates(inplace=True)
+dataset2.drop_duplicates(inplace=True)
+dataset3.drop_duplicates(inplace=True)
+
+dataset12 = pd.concat([dataset1,dataset2],axis=0)
+
+dataset1_y = dataset1.label
+dataset1_x = dataset1.drop(['User_id','label','day_gap_before','day_gap_after', 'Discount_rate', 'avg_user_date_datereceived_gap'],axis=1)  # 'day_gap_before','day_gap_after' cause overfitting, 0.77
+dataset2_y = dataset2.label
+dataset2_x = dataset2.drop(['User_id','label','day_gap_before','day_gap_after', 'Discount_rate', 'avg_user_date_datereceived_gap'],axis=1)
+dataset12_y = dataset12.label
+dataset12_x = dataset12.drop(['User_id','label','day_gap_before','day_gap_after', 'Discount_rate', 'avg_user_date_datereceived_gap'],axis=1)
+dataset3_preds = dataset3[['User_id','Coupon_id','Date_received']]
+dataset3_x = dataset3.drop(['User_id','Coupon_id','Date_received','day_gap_before','day_gap_after', 'Discount_rate', 'user_date_datereceived_gap'],axis=1)
+
+print (dataset1_x.shape,dataset2_x.shape,dataset3_x.shape)
+print(dataset3_x.head())
+
+dataset1 = xgb.DMatrix(dataset1_x,label=dataset1_y)
+dataset2 = xgb.DMatrix(dataset2_x,label=dataset2_y)
+dataset12 = xgb.DMatrix(dataset12_x,label=dataset12_y)
+dataset3 = xgb.DMatrix(dataset3_x)
+
+params={'booster':'gbtree',
+    'objective': 'rank:pairwise',
+    'eval_metric':'auc',
+    'gamma':0.1,
+    'min_child_weight':1.1,
+    'max_depth':5,
+    'lambda':10,
+    'subsample':0.7,
+    'colsample_bytree':0.7,
+    'colsample_bylevel':0.7,
+    'eta': 0.01,
+    'tree_method':'exact',
+    'seed':0,
+    'nthread':12, 
+    'silent': 1
+    }
+watch_list = [(dataset12, 'train')]
+
+model = xgb.train(params,dataset12,num_boost_round=1000, evals=watch_list)
+
+dataset3_preds['label'] = model.predict(dataset3)
+dataset3_preds.label = MinMaxScaler().fit_transform(dataset3_preds.label.reshape(-1, 1))
+dataset3_preds.sort_values(by=['Coupon_id','label'],inplace=True)
+dataset3_preds.to_csv("xgb_preds_0.8806.csv",index=None,header=None)
+print(dataset3_preds.describe())
+
+#save feature score
+feature_score = model.get_fscore()
+feature_score = sorted(feature_score.items(), key=lambda x:x[1],reverse=True)
+fs = []
+for (key,value) in feature_score:
+    fs.append("{0},{1}\n".format(key,value))
+    
+with open('xgb_feature_score.csv','w') as f:
+    f.writelines("feature,score\n")
+    f.writelines(fs)
+
